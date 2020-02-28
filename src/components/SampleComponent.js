a58d9f1c0983227cf7338220e13351c1b3ed156e
Jake Robbins <jrobbinsd@gmail.com>
2020-02-27 18:01:46 -0700

    and basic logic firing is happening

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default class Example2 extends React.Component {
 
+  constructor() {
+    super();
+    console.log('hi boys')
+  }
   render() {
    return(
      <span> hello world</span>
