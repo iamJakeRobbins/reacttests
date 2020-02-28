6980714a560ee4f98bf7492d3a3bb5106b05b5e2
Jake Robbins <jrobbinsd@gmail.com>
2020-02-27 18:25:11 -0700

    garbooo counter test is alive and well-ish

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -5,10 +5,38 @@ export default class Example2 extends React.Component {
   constructor() {
     super();
     console.log('hi boys')
+    this.state = {
+      num: 1
+    };
+    this.buttonClicker = this.buttonClicker.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
   }
+  handleMouseDown(e) {
+    this.buttonClicker();
+
+    e.stopPropagation();
+  }
+
+  buttonClicker() {
+    // this.state.num ++;
+    this.setState({ // this is why react is garbo
+      num: this.state.num + 1
+    });
+    console.log(this.state.num);
+
+  }
+
+
+
   render() {
    return(
-     <span> hello world</span>
+     <div>
+       <span> hello world</span>
+       <button onClick={this.handleMouseDown}>this is a button</button>
+       <span> {this.state.num} </span>
+     </div>
    )
  }
+
+
 }
