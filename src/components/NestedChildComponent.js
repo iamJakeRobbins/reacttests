4675fa98506a2459507df06f5f831887bdc44353
Jake Robbins <jrobbinsd@gmail.com>
2020-02-29 15:52:28 -0700

    we can transform props to state which seems dangerous from a state management perspective

--- src/components/NestedChildComponent.js
+++ src/components/NestedChildComponent.js
@@ -3,11 +3,19 @@ import React from 'react';
 export default class NestedChildComponent extends React.Component {
   render() {
     return (
-      <div>i'm living inside the child!</div>
+      <div>
+        <div>i'm living inside the child!</div>
+        <div>{this.props.headProps}</div>
+        <div>{this.state.transformPropsToState}</div>
+        </div>
+
     )
   }
   constructor(state, props) {
     super(state, props);
+    this.state = {
+      transformPropsToState: this.props.headProps,
+    };
     console.log(this.props)
   }
 }
